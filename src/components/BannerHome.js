851e749d62928119622847f1729ee40b9f320a20
Venugopal <karthivenu49@gmail.com>
2024-07-10 14:54:12 +0530

    Third message

--- src/components/BannerHome.js
+++ src/components/BannerHome.js
@@ -72,9 +72,11 @@ const BannerHome = () => {
                                 <p>View : { Number(data.popularity).toFixed(0)}</p>
 
                                 </div>
-                                <Link to={"/"+data?.media_type+"/"+data.id} className='bg-white px-4 py-2 text-black font-bold rounded mt-4 hover:bg-gradient-to-l from-red-700 to-orange-500 shadow-md transition-all hover:scale-105 '>
+                              <Link to={"/"+data?.media_type+"/"+data.id}>
+                              <button  className='block bg-white px-4 py-2 text-black font-bold rounded mt-4 hover:bg-gradient-to-l from-red-700 to-orange-500 shadow-md transition-all hover:scale-105 '>
                                     Play Now
-                                </Link>
+                                </button>
+                              </Link>
                         </div>
                         </div>
                        
